f5f174f8d825ab7b5a6a575838d9143c741bffeb
Khanh <uefa.euro30@gmail.com>
2021-09-20 11:46:41 +0700

    ABC

---
+++ buoi7_AbtractClass_Interface/thuchanh/Animal_and_Edible/src/animal/Chicken.java
@@ -0,0 +1,8 @@
+package animal;
+import
+public class Chicken extends Animal{
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck!";
+    }
+}
---
+++ buoi7_AbtractClass_Interface/thuchanh/Animal_and_Edible/src/animal/Tiger.java
@@ -0,0 +1,9 @@
+package animal;
+
+public class Tiger extends Animal {
+    @Override
+    public String makeSound() {
+        return "Tiger: roarrrrr!";
+    }
+}
+
